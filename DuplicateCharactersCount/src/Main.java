7fac76dcfa18285983682d84e3e47c84bedb672e
sethgreen23 <dridi.chaith@gmail.com>
2020-12-24 17:26:52 +0100

    commenting the first challenge

--- DuplicateCharactersCount/src/Main.java
+++ DuplicateCharactersCount/src/Main.java
@@ -6,13 +6,22 @@ public class Main {
       System.out.println(countDuplicatedNumbers(word));
     }
     public static int countDuplicatedNumbers(String word) {
+    	//create memorization object 
+    	//it is a map
     	Map<String,Integer> map = new HashMap<String,Integer>();
         int count=0;
+        //iterate throw the word
         for(int i=0;i<word.length();i++){
+        	//get each character in a form of String
             String letter = Character.toString(word.charAt(i));
+            //if the letter is not in the map
+            //we create an entry for it with value 1
             if(map.get(letter)==null){
                 map.put(letter,1);
             }else{
+            	//each time we find a character a second time
+            	//we increment the count 
+            	//with that we will have the number of duplicated caracter
                 if(map.get(letter)==1)
                     count++;
                 map.replace(letter,map.get(letter)+1);
